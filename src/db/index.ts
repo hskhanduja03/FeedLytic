f0586abdada8bb86f2b99e973757df387f100d80
Harmeet Singh <hskhanduja03@gmail.com>
2025-07-13 01:36:15 +0530

    eslint updates

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,8 +1,8 @@
 import { drizzle } from "drizzle-orm/neon-http";
-import { neon, neonConfig } from "@neondatabase/serverless";
+import { neon, NeonConfig, neonConfig } from "@neondatabase/serverless";
 import { Feedbacks } from "./schema";
 
-(neonConfig as any).fetch = fetch;
+(neonConfig as NeonConfig).fetch = fetch;
 const sql = neon(process.env.DATABASE_URL!); // uses WebSockets
 export const db = drizzle(sql, {
   schema: { Feedbacks },
